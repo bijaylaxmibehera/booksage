89e746e4b78f2e06aa16d8432c282549b9603fa8
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-05-31 15:52:11 +0530

    App: add mockman

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import {Routes, Route} from "react-router-dom";
+import Mockman from "mockman-js";
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Routes>
+     <Route path="/mockman" element={<Mockman />} />
+     </Routes>
     </div>
   );
 }
