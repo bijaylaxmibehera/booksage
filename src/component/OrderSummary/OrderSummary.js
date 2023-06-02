ee863e0103d730574490c5311836d6a8c31f4bfb
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-06-02 17:59:09 +0530

    style: add style to order summary component

--- src/component/OrderSummary/OrderSummary.js
+++ src/component/OrderSummary/OrderSummary.js
@@ -1,29 +1,50 @@
-import {useContext} from "react";
-import { CartContext } from "../../";
-import "./OrderSummary.css"
+import { useContext } from 'react'
+import { CartContext } from '../../'
+import './OrderSummary.css'
 
 export function OrderSummary () {
-    const {cartState}=useContext(CartContext);
-    const cartItems=cartState.items;
-    // console.log(cartItems);
-    const deliveryCharges=200;
-    const totalItems=cartItems.reduce((noOfItems,currItem)=>noOfItems+=currItem.quantity,0);
-    const totalPrice=cartItems.reduce((total,currItem)=>total+=currItem.price*currItem.quantity,0)
-    const totalDiscount=cartItems.reduce((total,currItem)=>total+=currItem.originalPrice*currItem.quantity,0)
-    const totalAmount=totalDiscount+deliveryCharges;
+  const { cartState } = useContext(CartContext)
+  const cartItems = cartState.items
+  // console.log(cartItems);
+  const deliveryCharges = 200
+  const totalItems = cartItems.reduce(
+    (noOfItems, currItem) => (noOfItems += currItem.quantity),
+    0
+  )
+  const totalPrice = cartItems.reduce(
+    (total, currItem) => (total += currItem.price * currItem.quantity),
+    0
+  )
+  const totalDiscount = cartItems.reduce(
+    (total, currItem) => (total += currItem.originalPrice * currItem.quantity),
+    0
+  )
+  const totalAmount = totalDiscount + deliveryCharges
 
   return (
     <>
-      <div>
-        <p>Price Details</p>
-        <hr/>
-        <p>Price ({totalItems} items): ₹{totalPrice}</p>
-        <p>Discount: -₹{totalDiscount}</p>
-        <p>Delivery charges: ₹{deliveryCharges}</p>
-        <hr/>
-        <p>Total Amount ₹{totalAmount}</p>
+      <div className='order-summary'>
+        <p className='div-heading'>Price Details</p>
+        <hr />
+        <div className='price-details'>
+            <p>Price ({totalItems} items):</p>
+            <p>₹{totalPrice}</p>
+        </div>
+        <div className='price-details'>
+            <p>Discount:</p>
+            <p>-₹{totalDiscount}</p>
+        </div>
+        <div className='price-details'>
+            <p>Delivery charges:</p>
+            <p>₹{deliveryCharges}</p>
+        </div>
+        <hr />
+        <div className='price-details total-amount'>
+            <p>Total Amount</p>
+            <p>₹{totalAmount}</p>
+        </div>
         <p>You will save ₹{totalDiscount} on this order.</p>
-        <button>Place Order</button>
+        <button className='order-btn'>Place Order</button>
       </div>
     </>
   )
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,15 +1,15 @@
-import { useContext } from 'react'
-import { CartContext } from '../../'
-import { CalculateDiscount } from '../../utils/CalculateDiscount'
+import { useContext } from 'react';
+import { CartContext } from '../../';
+import { CalculateDiscount } from '../../utils/CalculateDiscount';
 
-import "./Cart.css"
-import { OrderSummary } from '../../component/OrderSummary/OrderSummary'
+import "./Cart.css";
+import { OrderSummary } from '../../component/OrderSummary/OrderSummary';
 
-export function Cart () {
-  const { cartState, cartDispatch } = useContext(CartContext)
-  const cartItem = cartState.items
+export function Cart() {
+  const { cartState, cartDispatch } = useContext(CartContext);
+  const cartItem = cartState.items;
 
-    const handleRemoveFromCart = (item) => {
+  const handleRemoveFromCart = (item) => {
     cartDispatch({ type: 'REMOVE_FROM_CART', payload: item });
   };
 
@@ -26,35 +26,58 @@ export function Cart () {
   return (
     <>
       <h3 style={{ textAlign: 'center' }}>My Cart({cartItem.length})</h3>
-      {cartItem.map(item => {
-        const { _id: id, img, name, price, originalPrice ,quantity} = item
-        return (
-          <div key={id} className='product-card'>
-            <div className='product-img'>
-              <img src={img} alt={name} />
-            </div>
-            <div className='product-details'>
-              <h3>{name}</h3>
-              <div style={{display:"flex",justifyContent:"space-evenly", fontWeight:"bold", width:"100%"}} >
-              <p className='disc-price'>₹{price}</p>
-              <p className='actual-price'>₹{originalPrice}</p>
+      {cartItem.length === 0 ? (
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"70vh", fontWeight:"var(--font-semiBold)", fontSize:"1.2rem"}}>
+        <p> Hey your cart looks empty. Please add some item...</p>
+        </div>
+        
+       
+      ) : (
+        cartItem.map((item) => {
+          const { _id: id, img, name, price, originalPrice, quantity } = item;
+          return (
+            <div key={id} className='product-card'>
+              <div className='product-img'>
+                <img src={img} alt={name} />
               </div>
-              <p className="price-percentage"><CalculateDiscount price={price} originalPrice={originalPrice}/>% Off</p>
-              <p className='product-quantity'>
-                Quantity:
-                <button onClick={()=>handleDecreaseQuantity(item)}>-</button>
-                <span>{quantity}</span>
-                <button onClick={()=>handleIncreaseQuantity(item)}>+</button>
-              </p>
-              <div className='product-manage-btn'>
-              <button onClick={()=>handleRemoveFromCart(item)}>Remove from cart</button>
-              <button>Move to wishlist</button>
+              <div className='product-details'>
+                <h3>{name}</h3>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                    fontWeight: 'bold',
+                    width: '100%',
+                  }}
+                >
+                  <p className='disc-price'>₹{price}</p>
+                  <p className='actual-price'>₹{originalPrice}</p>
+                </div>
+                <p className='price-percentage'>
+                  <CalculateDiscount
+                    price={price}
+                    originalPrice={originalPrice}
+                  />
+                  % Off
+                </p>
+                <p className='product-quantity'>
+                  Quantity:
+                  <button onClick={() => handleDecreaseQuantity(item)}>-</button>
+                  <span>{quantity}</span>
+                  <button onClick={() => handleIncreaseQuantity(item)}>+</button>
+                </p>
+                <div className='product-manage-btn'>
+                  <button onClick={() => handleRemoveFromCart(item)}>
+                    Remove from cart
+                  </button>
+                  <button>Move to wishlist</button>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      })}
-      <OrderSummary />
+          );
+        })
+      )}
+      {cartItem.length !== 0 && <OrderSummary />}
     </>
-  )
+  );
 }
