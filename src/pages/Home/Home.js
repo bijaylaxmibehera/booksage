5b0af362a76d5d038f6beb67a408de32b5000030
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-06-06 16:18:35 +0530

    feat: add landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,44 @@
-import "./Home.css"
-export function Home(){
-    return (
-        <>
-        {/* <h1>This is home page</h1> */}
-        
-    
-        </>
-    )
+import './Home.css'
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { DataContext, FilterContext } from '../../'
+export function Home () {
+  const { categories } = useContext(DataContext)
+  const { filters, dispatchFilters } = useContext(FilterContext)
+
+  const setCategory = category => {
+    dispatchFilters({ type: 'ADD_CATEGORY', payload: category })
+  }
+
+  return (
+    <>
+      <section className='responsive-grid category-card'>
+        {categories.map(category => (
+          <>
+            <div key={category} className='category-card-overlay'>
+              <button onClick={() => setCategory(category)}>
+                <NavLink to='/products'>{category}</NavLink>
+              </button>
+            </div>
+          </>
+        ))}
+      </section>
+      <main>
+        <div className='background-overlay'></div>
+        <div className='main-text'>
+          <p>Welcome to BookSage!</p>
+          <h1>
+            Discover a world of knowledge and inspiration through our vast
+            collection of books.
+          </h1>
+          <button className='btn secondary shop-btn'>
+            <NavLink to='/products'>
+              Browse Collection
+              <i class='fa fa-chevron-right' aria-hidden='true'></i>
+            </NavLink>
+          </button>
+        </div>
+      </main>
+    </>
+  )
 }
