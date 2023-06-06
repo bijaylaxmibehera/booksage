01aac532ddeafc53ace7a99e8596e53c35a6a10e
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-06-06 09:51:32 +0530

    style: add styles to nav-bar

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,21 +1,39 @@
-import "./Header.css"
-import { NavLink } from "react-router-dom"
-export function Header(){
-    
-    return (
-       <>
-       <nav>
-        <div className="nav-left">
-           <NavLink to="/">BookSage</NavLink>
+import './Header.css'
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { CartContext,WishlistContext } from '../../'
+export function Header () {
+ 
+   const {cartState}=useContext(CartContext);
+   const {wishlistState}=useContext(WishlistContext)
+
+
+  return (
+    <>
+      <nav>
+        <div className='nav-left'>
+          <h3>
+            <NavLink to='/'>BookSage</NavLink>
+          </h3>
         </div>
-        <div className="nav-right">
-             <NavLink to="/" className="nav-list">Home</NavLink>
-             <NavLink to="/products" className="nav-list">Products</NavLink>
-             <NavLink to="/wishlist" className="nav-list">wishlist</NavLink>
-             <NavLink to="/cart" className="nav-list">Cart</NavLink>
-             <button><NavLink to="/">Login</NavLink></button>
+        <div className='nav-right'>
+          <NavLink to='/' className='nav-list'>
+          <i class="fa fa-home" aria-hidden="true"></i>
+          </NavLink>
+          {/* <NavLink to="/products" className="nav-list">Products</NavLink> */}
+          <NavLink to='/wishlist' className='badge nav-list'>
+            <div className='wishlist'>{wishlistState.wishlists.length}</div>
+            <i className='fa fa-heart' aria-hidden='true'></i>
+          </NavLink>
+          <NavLink to='/cart' className='badge nav-list'>
+            <div>{cartState.items.length}</div>
+            <i class='fa fa-shopping-cart' aria-hidden='true'></i>
+          </NavLink>
+          <button className="login-button">
+            <NavLink to='/login'>Login</NavLink>
+          </button>
         </div>
-       </nav>
-       </>
-    )
+      </nav>
+    </>
+  )
 }
